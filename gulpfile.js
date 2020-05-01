c66dcaf4d794c20c5ca53292569080b07b510c1b
wrgraff <wrgraff@gmail.com>
2020-05-01 04:01:50 +0300

    ready to deploy

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,77 @@
-'use strict';
- 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var cssmin = require('gulp-cssmin')
- 
-gulp.task('sass', function () {
-  gulp.src('./templates/flover/scss/style.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(cssmin())
-    .pipe(gulp.dest('./templates/flover/css'));
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+	plumber = require('gulp-plumber'),
+	csso = require('gulp-csso'),
+	postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer'),
+    rename = require('gulp-rename'),
+	imagemin = require('gulp-imagemin'),
+	del = require('del'),
+    browserSync = require('browser-sync').create();
+
+gulp.task('scss', () => {
+	return gulp.src('src/scss/style.scss')
+		.pipe(plumber())
+		.pipe(sass())
+		.pipe(postcss([ autoprefixer() ]))
+        .pipe(gulp.dest('dist/templates/flover/css/'))
+        .pipe(browserSync.stream())
+		.pipe(csso())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('dist/templates/flover/css/'));
 });
- 
-gulp.task('sass:watch', function () {
-  gulp.watch('./templates/flover/scss/**/*.scss', ['sass']);
+
+gulp.task('html', () => {
+	return gulp.src('src/html/**/*.html')
+        .pipe(gulp.dest('dist/'));
 });
+
+gulp.task('js', () => {
+	return gulp.src('src/js/**/*.js')
+        .pipe(gulp.dest('dist/templates/flover/js'));
+});
+
+gulp.task('fonts', () => {
+	return gulp.src('src/fonts/**/*')
+        .pipe(gulp.dest('dist/templates/flover/fonts'));
+});
+
+gulp.task('img', () => {
+	return gulp.src('src/img/**/*{jpg,png,svg}')
+		.pipe(imagemin([
+			imagemin.svgo({
+				plugins: [
+					{cleanupIDs: true}
+				]
+			})
+		]))
+        .pipe(gulp.dest('dist/templates/flover/img'));
+});
+
+gulp.task('del', () => {
+	return del('dist');
+});
+
+gulp.task('serve', () => {
+	browserSync.init({
+        server: "dist"
+    });
+
+    gulp.watch('src/scss/**/*.scss', gulp.series('scss'));
+});
+
+gulp.task('build', gulp.series(
+	'del',
+	'scss',
+	'html',
+	'js',
+	'img',
+	'fonts'
+));
+
+gulp.task('start', gulp.series(
+	'build',
+	'serve'
+));
+
+gulp.task('default', gulp.series('serve'));
